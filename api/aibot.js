32f0fd8b26ba0fbc7dbee50ec3a5735d33ede3a9
TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>
2026-09-17 13:06:02 +0700

    Update aibot.js

--- api/aibot.js
+++ api/aibot.js
@@ -35,14 +35,16 @@ const CUSTOM_PERSONALITY = process.env.BOT_PERSONALITY_AI || "";
 
 const GEMINI_MODELS = [
   "gemini-2.5-flash-lite",
-  "gemini-2.5-flash",
-  "gemini-3-flash-preview",
   "gemini-3.1-flash-lite",
   "gemini-3.5-flash-lite",
+  "gemini-2.5-flash",
+  "gemini-3-flash-preview",
   "gemini-3.5-flash",
   "gemini-3.6-flash",
   "gemini-3.7-flash",
-  "gemini-3.8-flash"
+  "gemini-3.8-flash",
+  "gemini-2.5-pro",
+  "gemini-3.1-pro-preview"
 ];
 
 let currentModelIndex = 0;
